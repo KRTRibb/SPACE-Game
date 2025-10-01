ea3675a74388e6c565c71ae01938b347148fa82e
Kieran Tribble <kai@macbook-air-8.home>
2025-10-01 17:57:01 +0100

    IDK bro

--- src/core/mathUtils.cpp
+++ src/core/mathUtils.cpp
@@ -3,17 +3,20 @@
 #include "core/mathUtils.hpp"
 
 
-Vector2 math::NormalizeVec(const Vector2& v) {
-    float len = sqrtf(v.x*v.x + v.y * v.y);
-    if (len < 1e-6) return {0.0f, 0.0f};
 
-    return {v.x / len, v.y / len};
-}
+namespace math {
+    Vector2 NormalizeVec(const Vector2& v) {
+        float len = sqrtf(v.x*v.x + v.y * v.y);
+        if (len < 1e-6) return {0.0f, 0.0f};
 
-float math::Dot(const Vector2& a, const Vector2& b){
-    return a.x * b.x + a.y * b.y;
-}
+        return {v.x / len, v.y / len};
+    }
 
-float math::CrossZ(const Vector2& a, const Vector2& b){
-    return a.x * b.y - a.y * b.x;
+    float Dot(const Vector2& a, const Vector2& b){
+        return a.x * b.x + a.y * b.y;
+    }
+
+    float CrossZ(const Vector2& a, const Vector2& b){
+        return a.x * b.y - a.y * b.x;
+    }
 }
